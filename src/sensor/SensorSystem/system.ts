a0b608bd8733a845385107276c88d36e19f3a0f7
tobber00 <39513708+tobber00@users.noreply.github.com>
2023-11-07 14:21:37 +0100

    script done maybe?

---
+++ src/sensor/SensorSystem/system.ts
@@ -0,0 +1,35 @@
+import { type EachMessagePayload, Kafka, Consumer } from 'kafkajs';
+
+const kafka = new Kafka({
+    clientId: 'sensor-consumer',
+    brokers: ['kafka:9092'],
+});
+
+const consumer: Consumer = kafka.consumer({ groupId: 'sensor-group' });
+
+const handleMessage = async ({ topic, partition, message }: EachMessagePayload): Promise<void> => {
+    if (topic === 'alert') {
+        (document.getElementById('alertText') as HTMLParagraphElement).textContent = "ALERT";
+    } else {
+        (document.getElementById('alertText') as HTMLParagraphElement).textContent = "";
+    }
+
+    await consumer.commitOffsets([{ topic, partition, offset: message.offset }]);
+};
+
+const runConsumer = async (): Promise<void> => {
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'alert' });
+
+    await consumer.run({
+        eachMessage: handleMessage,
+    });
+};
+
+runConsumer()
+    .then(() => {
+        console.log('Consumer is running...');
+    })
+    .catch((error) => {
+        console.error('Failed to run kafka consumer', error);
+    });
